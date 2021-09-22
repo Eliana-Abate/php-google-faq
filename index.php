70156ca0fdb80500c31bbd76f5d537374c61b8b4
Eliana-Abate <eliana.abate@outlook.it>
2021-09-22 16:01:22 +0200

    printed questions

--- index.php
+++ index.php
@@ -1,3 +1,22 @@
+<?php 
+
+    $google_faq = [
+        ['question' => 'Come state implementando la recente decisione della Corte di giustizia dell\'Unione europea (CGUE) relativa al diritto all\'oblio?',
+        'answer' => '###'],
+        ['question' => 'Come fa Google a proteggere la mia privacy e a tenere le mie informazioni al sicuro?',
+        'answer' => '###'],
+        ['question' => 'Perché il mio account è associato a un paese?',
+        'answer' => '###'],
+        ['question' => 'Come faccio a rimuovere informazioni su di me dai risultati di ricerca di Google?',
+        'answer' => '###'],
+        ['question' => 'Quando faccio clic sui risultati della Ricerca Google, le mie chiavi di ricerca vengono inviate ai siti web?',
+        'answer' => '###'],
+    ]
+
+?>
+
+
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -9,7 +28,13 @@
 <body>
     <header></header>
     <main>
-        
+        <?php foreach ($google_faq as $faq) { ?>
+            <section>
+                <h2><?php echo $faq['question'] ?></h2>
+                <p><?php echo $faq['answer'] ?></p>
+            </section>
+        <?php } ?>
+
     </main>
     <footer></footer>
 </body>
