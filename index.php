ddd2edd27b3228f250b61619fe860e1621e06eb4
Eliana-Abate <eliana.abate@outlook.it>
2021-09-22 16:52:54 +0200

    added style to <main> and its child

--- index.php
+++ index.php
@@ -24,10 +24,15 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Google FAQ</title>
+    <!-- Roboto - Google Font -->
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500&display=swap" rel="stylesheet">
+    <link rel="stylesheet" href="./css/style.css">
 </head>
 <body>
     <header></header>
-    <main>
+    <main class="container">
         <?php foreach ($google_faq as $faq) { ?>
             <section>
                 <?php echo $faq['question'] ?>
